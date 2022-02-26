3bc45a0769cf844b3aa238cde63ffc02a3799ec3
kaviprakashm <kaviprakashm@gmail.com>
2022-02-26 11:42:03 -0800

    Added 3 new model for ML

--- model_code.py
+++ model_code.py
@@ -1,3 +1,12 @@
 # This is model code for Python
 
 print("This is model code for Python new branching")
+
+def model1():
+    print("This is code for model1")
+
+def model2():
+    print("This is code for model2")
+
+def model3():
+    print("This is code for model3)
